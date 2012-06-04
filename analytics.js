367e7117eec3d3325cc64b1d4740e045cf78c081
robbles <odwyerrob@gmail.com>
2012-06-04 14:13:35 -0700

    new code organization and using options

--- analytics.js
+++ analytics.js
@@ -1,6 +1,10 @@
 console.warn('analytics loaded');
 var rules, events, options;
 
+var optionSet = function(option) {
+  return option in options && options[option] === true;
+};
+
 // Load options from background script
 chrome.extension.sendRequest({type: "getOptions"}, function(data) {
   rules = JSON.parse(data.rules);
@@ -10,18 +14,16 @@ chrome.extension.sendRequest({type: "getOptions"}, function(data) {
   init();
 });
 
-var option_enabled = function(opt) {
-  return opt in options && options[opt] === true; 
-}
+
+/*
+    jquery.tabSlideOUt.js v1.3
+    By William Paoli: http://wpaoli.building58.com
+*/
+(function(e){e.fn.tabSlideOut=function(t){var n=e.extend({tabHandle:".handle",speed:300,action:"click",tabLocation:"left",topPos:"200px",leftPos:"20px",fixedPosition:!1,positioning:"absolute",pathToTabImage:null,imageHeight:null,imageWidth:null,onLoadSlideOut:!1},t||{});n.tabHandle=e(n.tabHandle);var r=this;n.fixedPosition===!0?n.positioning="fixed":n.positioning="absolute",document.all&&!window.opera&&!window.XMLHttpRequest&&(n.positioning="absolute"),n.pathToTabImage!=null&&n.tabHandle.css({background:"url("+n.pathToTabImage+") no-repeat",width:n.imageWidth,height:n.imageHeight}),n.tabHandle.css({display:"block",textIndent:"-99999px",outline:"none",position:"absolute"}),r.css({"line-height":"1",position:n.positioning});var i={containerWidth:parseInt(r.outerWidth(),10)+21+"px",containerHeight:parseInt(r.outerHeight(),10)+"px",tabWidth:parseInt(n.tabHandle.outerWidth(),10)+"px",tabHeight:parseInt(n.tabHandle.outerHeight(),10)+"px"};if(n.tabLocation==="top"||n.tabLocation==="bottom")r.css({left:n.leftPos}),n.tabHandle.css({right:0});n.tabLocation==="top"&&(r.css({top:"-"+i.containerHeight}),n.tabHandle.css({bottom:"-"+i.tabHeight})),n.tabLocation==="bottom"&&(r.css({bottom:"-"+i.containerHeight,position:"fixed"}),n.tabHandle.css({top:"-"+i.tabHeight}));if(n.tabLocation==="left"||n.tabLocation==="right")r.css({height:i.containerHeight,top:n.topPos}),n.tabHandle.css({top:0});n.tabLocation==="left"&&(r.css({left:"-"+i.containerWidth}),n.tabHandle.css({right:"-"+i.tabWidth})),n.tabLocation==="right"&&(r.css({right:"-"+i.containerWidth}),n.tabHandle.css({left:"-"+i.tabWidth}),e("html").css("overflow-x","hidden")),n.tabHandle.click(function(e){e.preventDefault()});var s=function(){n.tabLocation==="top"?r.animate({top:"-"+i.containerHeight},n.speed).removeClass("open"):n.tabLocation==="left"?r.animate({left:"-"+i.containerWidth},n.speed).removeClass("open"):n.tabLocation==="right"?r.animate({right:"-"+i.containerWidth},n.speed).removeClass("open"):n.tabLocation==="bottom"&&r.animate({bottom:"-"+i.containerHeight},n.speed).removeClass("open")},o=function(){n.tabLocation=="top"?r.animate({top:"-3px"},n.speed).addClass("open"):n.tabLocation=="left"?r.animate({left:"-3px"},n.speed).addClass("open"):n.tabLocation=="right"?r.animate({right:"-3px"},n.speed).addClass("open"):n.tabLocation=="bottom"&&r.animate({bottom:"-3px"},n.speed).addClass("open")},u=function(){r.click(function(e){e.stopPropagation()}),e(document).click(function(){s()})},a=function(){n.tabHandle.click(function(e){r.hasClass("open")?s():o()}),u()},f=function(){r.hover(function(){o()},function(){s()}),n.tabHandle.click(function(e){r.hasClass("open")&&s()}),u()},l=function(){s(),setTimeout(o,500)};n.action==="click"&&a(),n.action==="hover"&&f(),n.onLoadSlideOut&&l()}})(jQuery);
+
 
 var init = function() {
   console.log(options);
-
-  if(option_enabled('solace-sidebar')) {
-      console.log('using sidebar');
-  } else {
-      console.log(' not using sidebar');
-    }
 
   // History tracking
   var history;
@@ -52,229 +54,43 @@ var init = function() {
       localStorage.history = JSON.stringify(history);
   }, 1000);
 
+
+  if(optionSet('solace-content-viewing') &&
+     window.location.host === 'solacesystems.com' &&
+     window.location.pathname === '/solutions/messaging-middleware/') {
+
+    console.log('solace-content-viewing enabled');
+    solace_content_viewing();
+
+  } else { console.log('solace-content-viewing disabled'); }
+
+  if(optionSet('solace-interest-detection') &&
+     window.location.host === 'solacesystems.com') {
+
+    console.log('solace-interest-detection enabled');
+    solace_interest_detection();
+
+  } else { console.log('solace-interest-detection disabled'); }
+
+  if(optionSet('solace-page-sets') &&
+     window.location.host === 'solacesystems.com') {
+
+    console.log('solace-page-sets enabled');
+    solace_page_sets();
+
+  } else { console.log('solace-page-sets disabled'); }
+
 };
 
 
-/*
-    tabSlideOUt v1.3
-    
-    By William Paoli: http://wpaoli.building58.com
+/*-----------------------------------------------------------------------------
 
-    To use you must have an image ready to go as your tab
-    Make sure to pass in at minimum the path to the image and its dimensions:
-    
-    example:
-    
-        $('.slide-out-div').tabSlideOut({
-                tabHandle: '.handle',                         //class of the element that will be your tab -doesnt have to be an anchor
-                pathToTabImage: 'images/contact_tab.gif',     //relative path to the image for the tab
-                imageHeight: '133px',                         //height of tab image
-                imageWidth: '44px',                           //width of tab image   
-        });
+   CONTENT VIEWING DETECTION
 
-    or you can leave out these options
-    and set the image properties using css
-    
-*/
+-----------------------------------------------------------------------------*/
+var solace_content_viewing = (function() {
 
-
-
-
-(function($){
-    $.fn.tabSlideOut = function(callerSettings) {
-        var settings = $.extend({
-            tabHandle: '.handle',
-            speed: 300, 
-            action: 'click',
-            tabLocation: 'left',
-            topPos: '200px',
-            leftPos: '20px',
-            fixedPosition: false,
-            positioning: 'absolute',
-            pathToTabImage: null,
-            imageHeight: null,
-            imageWidth: null,
-            onLoadSlideOut: false                       
-        }, callerSettings||{});
-
-        settings.tabHandle = $(settings.tabHandle);
-        var obj = this;
-        if (settings.fixedPosition === true) {
-            settings.positioning = 'fixed';
-        } else {
-            settings.positioning = 'absolute';
-        }
-        
-        //ie6 doesn't do well with the fixed option
-        if (document.all && !window.opera && !window.XMLHttpRequest) {
-            settings.positioning = 'absolute';
-        }
-        
-
-        
-        //set initial tabHandle css
-        
-        if (settings.pathToTabImage != null) {
-            settings.tabHandle.css({
-            'background' : 'url('+settings.pathToTabImage+') no-repeat',
-            'width' : settings.imageWidth,
-            'height': settings.imageHeight
-            });
-        }
-        
-        settings.tabHandle.css({ 
-            'display': 'block',
-            'textIndent' : '-99999px',
-            'outline' : 'none',
-            'position' : 'absolute'
-        });
-        
-        obj.css({
-            'line-height' : '1',
-            'position' : settings.positioning
-        });
-
-        
-        var properties = {
-                    containerWidth: parseInt(obj.outerWidth(), 10) + 21 + 'px',
-                    containerHeight: parseInt(obj.outerHeight(), 10) + 'px',
-                    tabWidth: parseInt(settings.tabHandle.outerWidth(), 10) + 'px',
-                    tabHeight: parseInt(settings.tabHandle.outerHeight(), 10) + 'px'
-                };
-
-        //set calculated css
-        if(settings.tabLocation === 'top' || settings.tabLocation === 'bottom') {
-            obj.css({'left' : settings.leftPos});
-            settings.tabHandle.css({'right' : 0});
-        }
-        
-        if(settings.tabLocation === 'top') {
-            obj.css({'top' : '-' + properties.containerHeight});
-            settings.tabHandle.css({'bottom' : '-' + properties.tabHeight});
-        }
-
-        if(settings.tabLocation === 'bottom') {
-            obj.css({'bottom' : '-' + properties.containerHeight, 'position' : 'fixed'});
-            settings.tabHandle.css({'top' : '-' + properties.tabHeight});
-            
-        }
-        
-        if(settings.tabLocation === 'left' || settings.tabLocation === 'right') {
-            obj.css({
-                'height' : properties.containerHeight,
-                'top' : settings.topPos
-            });
-            
-            settings.tabHandle.css({'top' : 0});
-        }
-        
-        if(settings.tabLocation === 'left') {
-            obj.css({ 'left': '-' + properties.containerWidth});
-            settings.tabHandle.css({'right' : '-' + properties.tabWidth});
-        }
-
-        if(settings.tabLocation === 'right') {
-            obj.css({ 'right': '-' + properties.containerWidth});
-            settings.tabHandle.css({'left' : '-' + properties.tabWidth});
-            
-            $('html').css('overflow-x', 'hidden');
-        }
-
-        //functions for animation events
-        
-        settings.tabHandle.click(function(event){
-            event.preventDefault();
-        });
-        
-        var slideIn = function() {
-            
-            if (settings.tabLocation === 'top') {
-                obj.animate({top:'-' + properties.containerHeight}, settings.speed).removeClass('open');
-            } else if (settings.tabLocation === 'left') {
-                obj.animate({left: '-' + properties.containerWidth}, settings.speed).removeClass('open');
-            } else if (settings.tabLocation === 'right') {
-                obj.animate({right: '-' + properties.containerWidth}, settings.speed).removeClass('open');
-            } else if (settings.tabLocation === 'bottom') {
-                obj.animate({bottom: '-' + properties.containerHeight}, settings.speed).removeClass('open');
-            }    
-            
-        };
-        
-        var slideOut = function() {
-            
-            if (settings.tabLocation == 'top') {
-                obj.animate({top:'-3px'},  settings.speed).addClass('open');
-            } else if (settings.tabLocation == 'left') {
-                obj.animate({left:'-3px'},  settings.speed).addClass('open');
-            } else if (settings.tabLocation == 'right') {
-                obj.animate({right:'-3px'},  settings.speed).addClass('open');
-            } else if (settings.tabLocation == 'bottom') {
-                obj.animate({bottom:'-3px'},  settings.speed).addClass('open');
-            }
-        };
-
-        var clickScreenToClose = function() {
-            obj.click(function(event){
-                event.stopPropagation();
-            });
-            
-            $(document).click(function(){
-                slideIn();
-            });
-        };
-        
-        var clickAction = function(){
-            settings.tabHandle.click(function(event){
-                if (obj.hasClass('open')) {
-                    slideIn();
-                } else {
-                    slideOut();
-                }
-            });
-            
-            clickScreenToClose();
-        };
-        
-        var hoverAction = function(){
-            obj.hover(
-                function(){
-                    slideOut();
-                },
-                
-                function(){
-                    slideIn();
-                });
-                
-                settings.tabHandle.click(function(event){
-                    if (obj.hasClass('open')) {
-                        slideIn();
-                    }
-                });
-                clickScreenToClose();
-                
-        };
-        
-        var slideOutOnLoad = function(){
-            slideIn();
-            setTimeout(slideOut, 500);
-        };
-        
-        //choose which type of action to bind
-        if (settings.action === 'click') {
-            clickAction();
-        }
-        
-        if (settings.action === 'hover') {
-            hoverAction();
-        }
-        
-        if (settings.onLoadSlideOut) {
-            slideOutOnLoad();
-        };
-        
-    };
-})(jQuery);
-
+// Add sidebar
 $(function(){
   $(document.body).append('<div class="slide-out-div">' + 
   '<a class="handle" href="http://link-for-non-js-users.html"><span>More Information</span></a>' +
@@ -315,12 +131,7 @@ $(function(){
 });
 
 
-
-(function() {
-
-// JavaScript Document
-// This code modifies page based on scrolling location
-
+// Modifies page based on scrolling location
 var y1 = 1200;
 var y2 = 1400;
 var flag = 0;
@@ -346,35 +157,56 @@ function outZone()
 // If flag == 1
 function scroll_hand()
 {
-	var e = window.event;
-	var buttom = window.scrollY + window.innerHeight;
-	//console.log("Window top = " + window.scrollY + "; Window buttom = " + buttom);
-	if((y1 > window.scrollY ) && ( y2 < (window.scrollY + window.innerHeight)))
-	{
-		if(flag === 0)
-		{
-			flag = 1;
-			
-			// Call function to react to being in the zone
-			inZone();
-			
-		}
-		else
-		{
-			//console.log("You are still in zone and flag = " + flag);
-			
-		};
-	}
-	else
-	{
-		flag = 0;
-		
-		// Call function to react to leaving the zone.
-		outZone();
-		//console.log("You are out of zone and flag = " + flag);
-	};
+  var e = window.event;
+  var buttom = window.scrollY + window.innerHeight;
+  //console.log("Window top = " + window.scrollY + "; Window buttom = " + buttom);
+  if((y1 > window.scrollY ) && ( y2 < (window.scrollY + window.innerHeight)))
+  {
+    if(flag === 0)
+    {
+      flag = 1;
+      
+      // Call function to react to being in the zone
+      inZone();
+      
+    }
+    else
+    {
+      //console.log("You are still in zone and flag = " + flag);
+      
+    };
+  }
+  else
+  {
+    flag = 0;
+    
+    // Call function to react to leaving the zone.
+    outZone();
+    //console.log("You are out of zone and flag = " + flag);
+  };
 
-	
+  
 }
 
-})();
+});
+
+/*-----------------------------------------------------------------------------
+
+   INTEREST DETECTION
+
+-----------------------------------------------------------------------------*/
+var solace_interest_detection = (function() {
+
+});
+
+
+
+/*-----------------------------------------------------------------------------
+
+   RELATED PAGE VISITS DETECTION
+
+-----------------------------------------------------------------------------*/
+var solace_page_sets = (function() {
+
+});
+
